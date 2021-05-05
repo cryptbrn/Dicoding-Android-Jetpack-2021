cbccdd8cd1e0e60febfdbb678ea4b1d0fa7b8915
cryptbrn <cryptbrnreyhan@apps.ipb.ac.id>
2021-05-05 23:31:31 +0700

    init commit

---
+++ app/src/main/java/com/example/filmify/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.filmify.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.filmify.R
+import com.example.filmify.adapter.HomePagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tabLayout=findViewById<TabLayout>(R.id.tab_layout)
+        val viewPager=findViewById<ViewPager2>(R.id.view_pager)
+
+        val adapter= HomePagerAdapter(supportFragmentManager,lifecycle)
+
+        viewPager.adapter=adapter
+
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            when(position){
+                0->{
+                    tab.text=getString(R.string.movies)
+                }
+                1->{
+                    tab.text=getString(R.string.tv_shows)
+                }
+            }
+        }.attach()
+    }
+}
